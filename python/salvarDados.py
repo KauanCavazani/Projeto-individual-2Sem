f59e8ae815f03782b132850416c1264d7902a2f3
KauanCavazani <kauan.cavazani@gmail.com>
2022-11-08 15:18:31 -0300

    add: script para salvar dados no banco

--- python/salvarDados.py
+++ python/salvarDados.py
@@ -1,7 +1,21 @@
 def main():
+    import pandas as pd
 
+    # bdsql, cursor = conectar()
+
+    with open("dadosCPU.csv", "r") as file:
+        file_csv = pd.read_csv(file, delimiter=";")
+        for i, row in enumerate(file_csv):
+            print(file_csv.loc[i])
+            # query = ("INSERT INTO leitura (fkMetrica, horario, valorLido, fkComponente_idComponente, fkComponente_fkSevidor) VALUES (%s, %s, %s, %s, %s);")
+            # val = ()                
 
 def conectar():
-    
+    import mysql.connector
+
+    bdsql = mysql.connector.connect(host="localhost", user="airdata_client", password="sptech", database="airData")
+    mycursor = bdsql.cursor()
+
+    return (bdsql, mycursor)
 
 main()
