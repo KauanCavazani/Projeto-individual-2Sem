0d68e296a4c4da9b340ce2a7f61e789b648ff848
KauanCavazani <kauan.cavazani@gmail.com>
2022-11-26 23:33:45 -0300

    add: novas amostras da cpu

--- R/script/scriptAmostra.R
+++ R/script/scriptAmostra.R
@@ -1,13 +1,14 @@
+cpuTemp <- sample(c(amostraTemp2$valorLido), size = 1170, replace = TRUE)
+
+
 idComponenteCPU <- amostraTemp$idComponente[1:390]
-macCPU <- amostraTemp$idServidor[1:390]
+macCPU <- amostraCPU2$idServidor[1:390]
 horario <- datas$V1
 valorCPU <- amostraTemp$valorLido[1:390]
 fkMetrica <- rep(4, 390)
 
-dadosTemp <- data.frame("fkMetrica"=c(fkMetrica),
-                      "idComponente"=c(idComponenteCPU),
-                       "idServidor"=c(macCPU),
+dadosTemp <- data.frame("idServidor"="10:5b:ad:fa:55:af",
                        "horario"=c(horario),
-                       "valorLido"=c(valorCPU))
+                       "valorLido"=c(cpuTemp))
 
-write.csv(dadosCPU,"~/Downloads/Projeto-individual-2Sem/dadosTemp.csv", row.names = FALSE)
+write.csv(dadosTemp,"~/Kauan/Github/Projeto-individual-2Sem/python/dadosTemp2.csv", row.names = FALSE)
