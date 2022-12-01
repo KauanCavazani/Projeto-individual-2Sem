4a5fe947dad1ed01ccf74a091ec925ca5b6b2e2e
KauanCavazani <kauan.cavazani@gmail.com>
2022-12-01 11:17:47 -0300

    update: script R

--- R/script/scriptAmostra.R
+++ R/script/scriptAmostra.R
@@ -1,14 +1,18 @@
-cpuTemp <- sample(c(amostraTemp2$valorLido), size = 1170, replace = TRUE)
+cpuPercent <- sample(c(dadosCPU$valorLido), size = 200, replace = TRUE)
+cpuPercent <- sample(c(cpuPercent), size = 394, replace = TRUE)
 
+cpuTemp <- sample(c(dadosTemp$valorLido), size = 200, replace = TRUE)
+cpuTemp <- sample(c(cpuTemp), size = 394, replace = TRUE)
 
-idComponenteCPU <- amostraTemp$idComponente[1:390]
-macCPU <- amostraCPU2$idServidor[1:390]
 horario <- datas$V1
-valorCPU <- amostraTemp$valorLido[1:390]
-fkMetrica <- rep(4, 390)
 
-dadosTemp <- data.frame("idServidor"="10:5b:ad:fa:55:af",
+dadosCPU2 <- data.frame("idServidor"="16:a6:95:10:ef:9f",
+                        "horario"=c(horario),
+                        "valorLido"=c(cpuPercent))
+
+dadosTemp2 <- data.frame("idServidor"="16:a6:95:10:ef:9f",
                        "horario"=c(horario),
                        "valorLido"=c(cpuTemp))
 
-write.csv(dadosTemp,"~/Kauan/Github/Projeto-individual-2Sem/python/dadosTemp2.csv", row.names = FALSE)
+write.csv(dadosCPU2,"~/Kauan/Github/Projeto-individual-2Sem/python/dadosCPU2.csv", row.names = FALSE)
+write.csv(dadosTemp2,"~/Kauan/Github/Projeto-individual-2Sem/python/dadosTemp2.csv", row.names = FALSE)
